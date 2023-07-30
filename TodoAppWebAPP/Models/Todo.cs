d2887d53e0fd88da1147a16630805a4daeec64d5
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-07-29 23:20:08 -0400

    Removed Unnessary files like priracy page and error pager

---
+++ TodoAppWebAPP/Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoAppWebAPP.Models;
+
+public class Todo
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string RequestId { get; internal set; }
+}
+
